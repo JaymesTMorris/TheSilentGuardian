b4f64e00942e8984b0404928e2cb51aaf1753e90
Safia Nassiri <niejelandrew@gmail.com>
2024-11-19 14:31:38 -0500

    Main Menu functions completed.

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_start_pressed() -> void:
+	print("Start Button Pressed!")
+	get_tree().change_scene_to_file("res://Scenes/Levels/Level1.tscn")
+
+
+
+func _on_quit_pressed() -> void:
+	print("Quit Button Pressed!")
+	get_tree().quit()
