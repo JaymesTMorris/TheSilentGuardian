ea24a8a319f47d4ba40f1d4583dca42266cd8568
Jaymes T Morris <Jaymestmorris16@gmail.com>
2024-11-20 19:52:11 -0500

    Update SpawnManager
    Fixed bug, highlightLayer returning null instance

--- Scripts/Singletons (Autoloads)/SpawnManager.gd
+++ Scripts/Singletons (Autoloads)/SpawnManager.gd
@@ -2,13 +2,12 @@ extends Node
 
 @export var spawnRate: float = 1.0
 @export var maxSpirits: int = 10
-@onready var highlightLayer: TileMapLayer = get_node("/root/Main/Map/HighlightLayer")
 
 @onready var bunnyScene: PackedScene = load("res://Scenes/BunnySpirit.tscn")
 func spawnSpirit() -> void:
 	# Implement spirit spawning logic
 	var instance: Node = bunnyScene.instantiate()
-	var cellLocation: Vector2i = highlightLayer.highlighted_cell
+	var cellLocation: Vector2i = highlightLayer().highlighted_cell
 	instance.position = Vector2(cellLocation.x * cell_size.x, cellLocation.y * cell_size.y)
 	instance.z_index = 8  # Set the z_index to so it spawns above the ground
 	add_child(instance)
@@ -18,7 +17,10 @@ var cell_size: Vector2 = Vector2(16, 16)  # Set cell size based on your grid
 
 func spawnTower() -> void:
 	var instance: Node = packedScene.instantiate()
-	var cellLocation: Vector2i = highlightLayer.highlighted_cell
+	var cellLocation: Vector2i = highlightLayer().highlighted_cell
 	instance.position = Vector2(cellLocation.x * cell_size.x, cellLocation.y * cell_size.y)
 	instance.z_index = 8  # Set the z_index to so it spawns above the ground
 	add_child(instance)
+
+func highlightLayer() -> TileMapLayer:
+	return get_node("/root/Main/Map/HighlightLayer")
